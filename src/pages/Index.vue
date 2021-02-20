f0395b35aff994c70bf77e2d08e7380ac2fd099a
ParkJsoo <misty0314@naver.com>
2021-02-21 02:32:21 +0900

    moment를 이용해 날짜 포맷하기
    ex) Sat, 20 Feb 2021 12:53:00 GM -> 5시간 전
    
    강의 내용
    - moment를 이용해 날짜 포맷하기

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -2,8 +2,8 @@
   <div>
     <q-list bordered>
       <q-item clickable v-ripple v-for="news in newsList" :key="news.link">
-        <q-item-section>{{ news.title }}</q-item-section>
-        <q-item-section>{{ news.pubDate }}</q-item-section>
+        <q-item-section class="text-h6">{{ news.title }}</q-item-section>
+        <q-item-section side class="text-caption">{{ fromNow(news.pubDate) }}</q-item-section>
       </q-item>
     </q-list>
   </div>
@@ -15,6 +15,10 @@ const Parser = require('rss-parser');
 const parser = new Parser();
 //cors policy 해결을 위해 사용(proxy 우회)
 const CORS_PROXY = "https://secret-ocean-49799.herokuapp.com"
+//날짜 형식을 위한 모듈
+const moment = require("moment")
+//언어를 한국어로 변경
+moment.locale("ko-kr")
 
 export default {
   data() {
@@ -26,7 +30,12 @@ export default {
     const response = await parser.parseURL(`${CORS_PROXY}/https://news.google.com/rss?hl=ko&gl=KR&ceid=KR:ko`)
     this.newsList = response.items
   },
-  
+  methods: {
+    //날짜 포맷을 지금으로부터 얼마전인지로 바꾸는 메소드
+    fromNow(timestamp) {
+      return moment(timestamp).fromNow()
+    }
+  }
 }
 </script>
 
