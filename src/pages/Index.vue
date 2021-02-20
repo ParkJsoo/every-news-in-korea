566d2fa492398b8dbc064ee97d28e2ce6670ba98
ParkJsoo <misty0314@naver.com>
2021-02-21 02:21:51 +0900

    뉴스 정보 가져와서 화면에 뿌리기
    axios로 뉴스 정보 가져와서 화면에 뿌리기
    
    강의 내용
    - axios로 뉴스 정보 가져오기
    - rss xml 데이터를 json으로 변환
    - 파싱된 정보를 화면에 뿌리기

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,11 +1,31 @@
 <template>
   <div>
-    <h1></h1>
+    <q-list bordered>
+      <q-item clickable v-ripple v-for="news in newsList" :key="news.link">
+        <q-item-section>{{ news.title }}</q-item-section>
+        <q-item-section>{{ news.pubDate }}</q-item-section>
+      </q-item>
+    </q-list>
   </div>
 </template>
 
 <script>
+//rss xml 데이터를 json으로 변경을 위해 사용
+const Parser = require('rss-parser');
+const parser = new Parser();
+//cors policy 해결을 위해 사용(proxy 우회)
+const CORS_PROXY = "https://secret-ocean-49799.herokuapp.com"
+
 export default {
+  data() {
+    return {
+      newsList: []
+    }
+  },
+  async created() {
+    const response = await parser.parseURL(`${CORS_PROXY}/https://news.google.com/rss?hl=ko&gl=KR&ceid=KR:ko`)
+    this.newsList = response.items
+  },
   
 }
 </script>
