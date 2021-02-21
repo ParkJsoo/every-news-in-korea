09de27422a94d6ab67f907ecd6adc2d321f717e5
ParkJsoo <misty0314@naver.com>
2021-02-21 23:29:40 +0900

    qtable 사용하기
    q-list 대신 q-table을 사용
    - template을 사용한 스타일 설정
    - filter를 사용한 검색 기능 활성화
    - 편리한 pagination
    - 링크 연결
    - quasar 자체에서 날짜 포맷 지원
    
    강의 내용
    - qtable을 이용해 화면에 뿌리기

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,44 +1,117 @@
 <template>
   <div>
-    <q-list bordered>
-      <q-item clickable v-ripple v-for="news in newsList" :key="news.link">
-        <q-item-section class="text-h6">{{ news.title }}</q-item-section>
-        <q-item-section side class="text-caption">{{ fromNow(news.pubDate) }}</q-item-section>
-      </q-item>
-    </q-list>
+    <q-table
+      flat
+      :data="newsList"
+      :columns="columns"
+      :filter="filter"
+      row-key="name"
+      @row-click="rowClick"
+      :pagination.sync="pagination"
+    >
+      <template v-slot:body-cell-title="props">
+        <q-td :props="props">
+          <div
+            class="text-h6 text-grey bg-purple-7"
+            style="white-space: normal"
+          >
+            {{ props.value }}
+          </div>
+          <div class="my-table-details">
+            {{ props.row.details }}
+          </div>
+        </q-td>
+      </template>
+      <template v-slot:body-cell-pubDate="props">
+        <q-td :props="props">
+          <div>
+            {{ formatDate(props.value) }}
+          </div>
+          <div class="my-table-details">
+            {{ props.row.details }}
+          </div>
+        </q-td>
+      </template>
+      <template v-slot:top-right>
+        <q-input
+          borderless
+          dense
+          debounce="300"
+          v-model="filter"
+          placeholder="검색"
+        >
+          <template v-slot:append>
+            <q-icon class="fab fa-searchengin" />
+          </template>
+        </q-input>
+      </template>
+    </q-table>
   </div>
 </template>
 
 <script>
 //rss xml 데이터를 json으로 변경을 위해 사용
-const Parser = require('rss-parser');
+const Parser = require("rss-parser");
 const parser = new Parser();
 //cors policy 해결을 위해 사용(proxy 우회)
-const CORS_PROXY = "https://secret-ocean-49799.herokuapp.com"
+const CORS_PROXY = "https://secret-ocean-49799.herokuapp.com";
 //날짜 형식을 위한 모듈
-const moment = require("moment")
+const moment = require("moment");
 //언어를 한국어로 변경
-moment.locale("ko-kr")
+moment.locale("ko-kr");
+// Quasar Date Utils
+import { date } from "quasar";
 
 export default {
   data() {
     return {
-      newsList: []
-    }
+      pagination: {
+        rowsPerPage: 20 // current rows per page being displayed
+      },
+      newsList: [],
+      filter: "",
+      columns: [
+        {
+          name: "title",
+          required: true,
+          label: "기사 제목",
+          align: "left",
+          field: row => row.title,
+          format: val => `${val}`,
+          sortable: true
+        },
+        {
+          name: "pubDate",
+          required: true,
+          label: "발행일",
+          align: "center",
+          field: row => row.pubDate,
+          format: val => `${val}`,
+          sortable: true
+        }
+      ]
+    };
   },
   async created() {
-    const response = await parser.parseURL(`${CORS_PROXY}/https://news.google.com/rss?hl=ko&gl=KR&ceid=KR:ko`)
-    this.newsList = response.items
+    const response = await parser.parseURL(
+      `${CORS_PROXY}/https://news.google.com/rss?hl=ko&gl=KR&ceid=KR:ko`
+    );
+    this.newsList = response.items;
   },
   methods: {
     //날짜 포맷을 지금으로부터 얼마전인지로 바꾸는 메소드
-    fromNow(timestamp) {
-      return moment(timestamp).fromNow()
+    // fromNow(timestamp) {
+    //   return moment(timestamp).fromNow();
+    // },
+    // Quasar Date Utils를 사용하여 날짜 포맷 변경
+    formatDate(timestamp) {
+      return date.formatDate(timestamp, "YYYY-MM-DD HH:MM");
+    },
+    rowClick(evt, row, index) {
+      window.open(this.newsList[index].link);
     }
   }
-}
+};
 </script>
 
-<style>
-
-</style>
+<style></style>
